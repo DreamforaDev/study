c21cf697407ee9fd69b686fdbfcba1cb32c7c769
Kosieun <sieun1156@naver.com>
2025-01-03 15:03:15 +0900

    refactor : 상영시간, 러닝타임 데이터 레이블 분리 및 데이터 수정
    refactor : list_movie.xml과 activity_movie_detail.xml의 상영시간과 러닝타임 레이블을 텍스트뷰로 분리하고 데이터를 수정하였습니다!

--- android-movie/app/src/main/java/com/example/movie/MainActivity.kt
+++ android-movie/app/src/main/java/com/example/movie/MainActivity.kt
@@ -19,59 +19,59 @@ class MainActivity : AppCompatActivity() {
             1,
             R.drawable.poster_1,
             "해리포터와 마법의 돌",
-            "상영일 : 2001-01-10~2001-03-01",
-            "러닝타임 : 180분"
+            "2001-01-10~2001-03-01",
+            "150분"
         ),
         ListItem.MovieItem(
             2,
             R.drawable.poster_2,
             "해리포터와 비밀의 방",
-            "상영일 : 2002-01-10~2002-03-01",
-            "러닝타임 : 180분"
+            "2002-01-10~2002-03-01",
+            "180분"
         ),
         ListItem.MovieItem(
             3,
             R.drawable.poster_3,
             "해리포터와 아즈카반의 죄수",
-            "상영일 : 2004-01-10~2004-03-01",
-            "러닝타임 : 180분"
+            "2004-01-10~2004-03-01",
+            "200분"
         ),
         ListItem.AdItem(R.drawable.baemin_ad, "https://www.dreamfora.com/"),
         ListItem.MovieItem(
             4,
             R.drawable.poster_4,
             "해리포터와 불의 잔",
-            "상영일 : 2005-01-10~2005-03-01",
-            "러닝타임 : 180분"
+            "2005-01-10~2005-03-01",
+            "198분"
         ),
         ListItem.MovieItem(
             5,
             R.drawable.poster_5,
             "해리포터와 불사조 기사단",
-            "상영일 : 2007-01-10~2007-03-01",
-            "러닝타임 : 180분"
+            "2007-01-10~2007-03-01",
+            "176분"
         ),
         ListItem.MovieItem(
             6,
             R.drawable.poster_6,
             "해리포터와 혼혈 왕자",
-            "상영일 : 2009-01-10~2009-03-01",
-            "러닝타임 : 180분"
+            "2009-01-10~2009-03-01",
+            "167분"
         ),
         ListItem.AdItem(R.drawable.toss_ad, "https://www.dreamfora.com/"),
         ListItem.MovieItem(
             7,
             R.drawable.poster_7,
             "해리포터와 죽음의 성물 I",
-            "상영일 : 2010-01-10~2010-03-01",
-            "러닝타임 : 180분"
+            "2010-01-10~2010-03-01",
+            "160분"
         ),
         ListItem.MovieItem(
             8,
             R.drawable.poster_8,
             "해리포터와 죽음의 성물 II",
-            "상영일 : 2011-01-10~2011-03-01",
-            "러닝타임 : 180분"
+            "2011-01-10~2011-03-01",
+            "190분"
         )
     )
 
